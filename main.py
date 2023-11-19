99b927be745a0e94195a5da3cbfa9c0d169bf8d5
ohadguetta <o4255542@gmail.com>
2023-11-19 21:10:23 +0200

    added send wotd subscribed user to firestore

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
 import logging
+
 # import re
 # import datetime
 import json
+import wordOfTheDay
 
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.constants import ParseMode
@@ -21,29 +23,29 @@ logging.basicConfig(  # if things not working properly
 )
 
 
-BOT_TOKEN = '6446448924:AAEWPivNKmRfsEdYVy0MZLKryOCD08fblPM' #DONE: add bot token
-DEFAULT_MESSAGE = 'Welcome to the Language Learner bot 🇮🇱! \n <b>Pick a language to learn:</b>'
+BOT_TOKEN = "6446448924:AAEWPivNKmRfsEdYVy0MZLKryOCD08fblPM"  # DONE: add bot token
+DEFAULT_MESSAGE = "Welcome to the Language Learner bot 🇮🇱! \n <b>Pick a language to learn:</b>"
+languages = ['Hebrew','Spanish']
 
 
+# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-#async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
 #    await context.bot.send_message(chat_id=update.effective_chat.id, text=messageToSend)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton(text=f'Hebrew 🇮🇱',callback_data=json.dumps({'language':'Hebrew'}))],
-        [InlineKeyboardButton("Spanish 🇪🇸", callback_data=json.dumps({'language': 'Spanish'}))],
+        [InlineKeyboardButton(text="Spanish 🇪🇸", callback_data=json.dumps({'language': 'Spanish'}))]
     ]
-
     markup_languages = InlineKeyboardMarkup(keyboard)
-
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=DEFAULT_MESSAGE,
-        reply_markup= markup_languages,
+        chat_id=update.effective_chat.id,
+        text=DEFAULT_MESSAGE,
+        reply_markup=markup_languages,
         parse_mode=ParseMode.HTML
     )
+
 
 async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -51,28 +53,55 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # Handle button clicks here
     data = json.loads(query.data)
-    chosen_language = data['language']
-    #TODO: subscribe user to word of the day in firebase firestore
-    #TODO: send the user a word of the day every day
-    await query.edit_message_text(text=f"You Clicked {chosen_language}")
+    chosen_language = data["language"]
+    # DONE: subscribe user to word of the day in firebase firestore
+    # TODO: send the user a word of the day every day
+    isSuccess = wordOfTheDay.subscribe_user(update.effective_chat.id, chosen_language)
+
+    if isSuccess:
+        await query.edit_message_text(
+            text=f"You subscribed successfully to the {chosen_language} language!"
+        )
+    else:
+        await query.edit_message_text(text=f"There was an error in the server!")
 
 
 
-if __name__ == "__main__":
-    application = (
-        ApplicationBuilder()
-        .token(BOT_TOKEN)
-        .build()
+
+async def send_wotd(update: Update,context: ContextTypes.DEFAULT_TYPE): #Sends word of the day to all users
+    wotdArr = {}
+    for language in languages:
+        wotdArr[language] = wordOfTheDay.getRandomWord(language)
+    subscribed_users = wordOfTheDay.pull_subscriptions()
+    for user in subscribed_users: #TODO: check for every user and its languages and send its word of the day
+        for language in user['subscriptions']:
+            try:
+                await context.bot.send_message(
+                chat_id= user['chat_id']
+                ,text=f'Translation to <b> {wotdArr[language]["word"]} </b> in {language} is: <b> {wotdArr[language]["translation"]} </b>' #TODO: add word of the day
+                # reply_markup= #TODO: maybe add a list of known words to user
+                ,parse_mode=ParseMode.HTML
+                )
+            except:
+                logging.warning('Error when sending wotd,user not found probably')
+    
+async def pull_subscriptions(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result = wordOfTheDay.pull_subscriptions()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=result, 
     )
 
 
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("send_word_of_the_day", send_wotd))
+    application.add_handler(CommandHandler("pull_subscriptions", pull_subscriptions))
     application.add_handler(CallbackQueryHandler(button_click))
 
     # application.add_handler(CommandHandler("archivereminders", archiveReminders))
-
-
 
     # job_queue = application.job_queue
 
--- wordOfTheDay.py
+++ wordOfTheDay.py
@@ -1,18 +1,33 @@
+import logging
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore,exceptions
+# from translate import Translator
 # from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 # from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 
 # Replace with your Firebase credentials JSON file
-cred = credentials.Certificate("path/to/your/firebase-credentials.json") #TODO: add firebase firestore
+cred = credentials.Certificate("serviceAccountKey.json") #DONE: add firebase firestore
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
+logging.basicConfig(  # if things not working properly
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
 def subscribe_user(user_id,language):
-    user_ref = db.collection('users').document(str(user_id))
-    user_ref.set({'subscriptions': firestore.ArrayUnion([language]) }) #TODO: check if user already subscribed before addition of language
+    try:
+        user_ref = db.collection('users').document(str(user_id))
+        # print(user_ref.get().to_dict())
+        if user_ref.get().exists:
+            user_ref.update({'subscriptions': firestore.ArrayUnion([language]) }) #DONE: check if user already subscribed before addition of language
+        else:
+            user_ref.set({'subscriptions': firestore.ArrayUnion([language]) }) 
+        return True
+    except exceptions.FirebaseError as e:
+        logging.warning('Server has returned an error',e)
+        return False
+
 
 
 # def send_word_of_the_day():
@@ -39,7 +54,17 @@ def pull_subscriptions():
 
     for user in subscriptions_ref:
         user_data = user.to_dict()
-        user_id = user.id
-        subscribed_users.append({'user_id': user_id, 'language': user_data.get('subscribed_languages', [])})
+        chat_id = user.id
+        subscribed_users.append({'chat_id': chat_id, 'subscriptions': user_data.get('subscriptions', [])})
     
     return subscribed_users
+
+def getRandomWord(language):
+    #TODO: get random word and return its translation to the languagea and the word itself
+    # translator= Translator(to_lang="eng")
+    # translation = translator.translate('לקחת')
+    if language == 'Hebrew':
+        # return {'word':{translation},'translation':'לקחת'}
+        return {'word':'Take','translation':'לקחת'}
+    elif language =='Spanish':
+        return {'word':'Take','translation':'llevar'}
